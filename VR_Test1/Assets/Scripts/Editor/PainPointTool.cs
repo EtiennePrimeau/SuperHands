488ae54f506a3d65acd1c23e0762173986ccfb78
EtiennePrimeau <1277029@bdeb.qc.ca>
2024-06-11 17:20:32 -0400

    new setup with Meta XR SDK

---
+++ VR_Test1/Assets/Scripts/Editor/PainPointTool.cs
@@ -0,0 +1,96 @@
+using System;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class PainPointTool : EditorWindow
+{
+    private enum PainPositionsHand
+    { Left, Right }
+    
+    private enum PainPositionsFinger
+    { Index, Middle, Ring, Pinky, Thumb }
+
+    private EnumField _handEnumField;
+    private EnumField _fingerEnumField;
+    private Button _button;
+
+
+
+    [MenuItem("Splendide/Pain Point Tool")]
+    public static void OpenWindow()
+    {
+        GetWindow<PainPointTool>();
+    }
+
+    private void CreateGUI()
+    {
+        
+        _handEnumField = new EnumField("Pain Position - Hand", PainPositionsHand.Left);
+        rootVisualElement.Add(_handEnumField);
+
+        _handEnumField.RegisterValueChangedCallback(evt =>
+        {
+            PainPositionsHand selectedValue = (PainPositionsHand)evt.newValue;
+        });
+
+        _fingerEnumField = new EnumField("Pain Position - Finger", PainPositionsFinger.Index);
+        rootVisualElement.Add(_fingerEnumField);
+
+        _fingerEnumField.RegisterValueChangedCallback(evt =>
+        {
+            PainPositionsFinger selectedValue = (PainPositionsFinger)evt.newValue;
+        });
+
+        _button = new Button();
+        _button.text = "Add Pain Point";
+        rootVisualElement.Add(_button);
+
+        _button.clicked += AddPainPoint;
+    }
+
+    private void AddPainPoint()
+    {
+        OVRSkeleton.SkeletonType hand;
+
+        switch (_handEnumField.value)
+        {
+            case PainPositionsHand.Left:
+                hand = OVRSkeleton.SkeletonType.HandLeft;
+                break;
+            case PainPositionsHand.Right:
+                hand = OVRSkeleton.SkeletonType.HandRight;
+                break;
+            default:
+                hand = OVRSkeleton.SkeletonType.None;
+                break;
+        }
+
+
+        OVRSkeleton.BoneId boneId;
+        switch (_fingerEnumField.value)
+        {
+            case PainPositionsFinger.Index:
+                boneId = OVRSkeleton.BoneId.Hand_IndexTip;
+                break;
+            case PainPositionsFinger.Middle:
+                boneId = OVRSkeleton.BoneId.Hand_MiddleTip;
+                break;
+            case PainPositionsFinger.Ring:
+                boneId = OVRSkeleton.BoneId.Hand_RingTip;
+                break;
+            case PainPositionsFinger.Pinky:
+                boneId = OVRSkeleton.BoneId.Hand_PinkyTip;
+                break;
+            case PainPositionsFinger.Thumb:
+                boneId = OVRSkeleton.BoneId.Hand_ThumbTip;
+                break;
+            default:
+                boneId = OVRSkeleton.BoneId.Invalid;
+                break;
+        }
+
+        PainPointToolManager.Instance.SpawnPainPoint(hand, boneId);
+        
+    }
+}
---
+++ VR_Test1/Assets/Scripts/PainPointToolManager.cs
@@ -0,0 +1,73 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PainPointToolManager : MonoBehaviour
+{
+    private static PainPointToolManager instance;
+
+    public static PainPointToolManager Instance
+    {
+        get
+        {
+            if (instance != null) return instance;
+
+            Debug.LogError("PainPointToolManager is null");
+            return null;
+        }
+        
+    }
+
+
+    [SerializeField] private OVRHand _rightHand;
+    [SerializeField] private OVRHand _leftHand;
+    [SerializeField] private OVRSkeleton _rightSkeleton;
+    [SerializeField] private OVRSkeleton _leftSkeleton;
+
+    [SerializeField] private GameObject _painPointPrefab;
+
+
+
+
+    public void SpawnPainPoint(OVRSkeleton.SkeletonType hand, OVRSkeleton.BoneId boneId)
+    {
+        if (hand == OVRSkeleton.SkeletonType.HandRight && !_rightHand.IsTracked)
+        {
+            Debug.Log("right hand is not tracked");
+            return;
+        }
+        if (hand == OVRSkeleton.SkeletonType.HandLeft && !_leftHand.IsTracked)
+        {
+            Debug.Log("left hand is not tracked");
+            return;
+        }
+
+
+        IList<OVRBone> bones;
+        switch (hand)
+        {
+            case OVRSkeleton.SkeletonType.HandLeft:
+                bones = _leftSkeleton.Bones;
+                break;
+            case OVRSkeleton.SkeletonType.HandRight:
+                bones = _rightSkeleton.Bones;
+                break;
+            case OVRSkeleton.SkeletonType.None:
+            case OVRSkeleton.SkeletonType.Body:
+            case OVRSkeleton.SkeletonType.FullBody:
+            default:
+                Debug.Log("SkeletonType is not a hand");
+                bones = null;
+                return;
+        }
+
+
+        foreach (var bone in bones)
+        {
+            if (bone.Id == boneId)
+            {
+                Instantiate(_painPointPrefab, bone.Transform);
+            }
+        }
+    }
+}
