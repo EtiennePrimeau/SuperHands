6c14f4d0391ac64da0da1656ead6a2f455fcd5c6
EtiennePrimeau <1277029@bdeb.qc.ca>
2024-07-09 18:02:49 -0400

    PainPoint V2.1 -- Demo version

--- VR_Test1/Assets/Scripts/V2/PainPointV2_1.cs
+++ VR_Test1/Assets/Scripts/V2/PainPointV2_1.cs
@@ -6,14 +6,11 @@ public class PainPointV2_1 : MonoBehaviour
     private OVRSkeleton.SkeletonType _hand;
     private OVRSkeleton.BoneId _boneId;
 
-    private Vector3 _startPos;
-    private float _detachDistance = 0.12f;
 
     public void Init(OVRSkeleton.SkeletonType hand, OVRSkeleton.BoneId boneId)
     {
         _hand = hand;
         _boneId = boneId;
-        _startPos = transform.position;
     }
 
     private void OnCollisionStay(Collision collision)
@@ -22,6 +19,8 @@ public class PainPointV2_1 : MonoBehaviour
         if (fingertip == null)
             return;
 
+        if (fingertip.Hand == _hand)
+            return;
 
         //if (fingertip.Hand == _hand)
         //{
@@ -32,8 +31,6 @@ public class PainPointV2_1 : MonoBehaviour
         //    DebugLogManager.Instance.PrintLog("same bone");
         //}
 
-        if (fingertip.Hand == _hand)
-            return;
 
 
         //DebugLogManager.Instance.PrintLog(OVRSkeleton.BoneLabelFromBoneId(fingertip.Hand, fingertip.BoneId));
@@ -48,11 +45,10 @@ public class PainPointV2_1 : MonoBehaviour
     {
 
         //DebugLogManager.Instance.PrintLog(Vector3.Distance(transform.position, _startPos).ToString());
-
-        if (Vector3.Distance(transform.position, _startPos) > _detachDistance)
-        {
-            //DebugLogManager.Instance.PrintLog("detaching");
-            //transform.SetParent(null);
-        }
+        //if (Vector3.Distance(transform.position, _startPos) > _detachDistance)
+        //{
+        //    DebugLogManager.Instance.PrintLog("detaching");
+        //    transform.SetParent(null);
+        //}
     }
 }
