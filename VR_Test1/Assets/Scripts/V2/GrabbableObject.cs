ffa32d27c9b1b478d771e18db13fe3bbb35cd420
EtiennePrimeau <1277029@bdeb.qc.ca>
2024-07-22 12:47:42 -0400

    Fixed velocity bugs when Releasing -- also moved GrabPlacement

--- VR_Test1/Assets/Scripts/V2/GrabbableObject.cs
+++ VR_Test1/Assets/Scripts/V2/GrabbableObject.cs
@@ -5,17 +5,32 @@ public class GrabbableObject : MonoBehaviour
     private FixedJoint _attachedFixedJoint;
     private Rigidbody _rb;
     private Vector3 _previousPos;
+    private Vector3 _velocity;
     private Material _material;
+
+    private Vector3 _startPos; // for debugging/resetting pos
 
     private void Start()
     {
         _rb = GetComponent<Rigidbody>();
         _material = GetComponent<MeshRenderer>().material;
+
+        _startPos = transform.position;
     }
 
     private void FixedUpdate()
     {
+        _velocity = (transform.position - _previousPos) / Time.fixedDeltaTime;
         _previousPos = transform.position;
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            transform.position = _startPos;
+            _rb.velocity = Vector3.zero;
+        }
     }
 
     private void OnTriggerEnter(Collider other)
@@ -48,18 +63,18 @@ public class GrabbableObject : MonoBehaviour
     {
         //DebugLogManager.Instance.PrintLog(gameObject.name + " is detaching");
 
-        Vector3 velocity = (transform.position - _previousPos) / Time.fixedDeltaTime;
-        DebugLogManager.Instance.PrintLog(velocity.ToString());
+        //Vector3 velocity = (transform.position - _previousPos) / Time.fixedDeltaTime;
+        DebugLogManager.Instance.PrintLog(_velocity.ToString());
 
         _attachedFixedJoint.connectedBody = null;
         _attachedFixedJoint = null;
 
-        _rb.velocity = velocity;
+        _rb.velocity = _velocity;
     }
 
     public void HighlightAsGrabbable()
     {
-        DebugLogManager.Instance.PrintLog("Highlighting");
+        //DebugLogManager.Instance.PrintLog("Highlighting");
 
         Color color = _material.color;
         color.a = 0.1f;
@@ -68,8 +83,7 @@ public class GrabbableObject : MonoBehaviour
 
     public void StopHighlight()
     {
-        DebugLogManager.Instance.PrintLog("stopping highlight");
-
+        //DebugLogManager.Instance.PrintLog("stopping highlight");
 
         Color color = _material.color;
         color.a = 1f;
