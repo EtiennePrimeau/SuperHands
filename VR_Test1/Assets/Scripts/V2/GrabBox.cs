7f58a237b52f72a2e8580d1fceb685d921be7ba0
EtiennePrimeau <1277029@bdeb.qc.ca>
2024-07-22 12:20:47 -0400

    GrabbableObject -- now changes visual (alpha) when is the closest Grabbable to the hand

--- VR_Test1/Assets/Scripts/V2/GrabBox.cs
+++ VR_Test1/Assets/Scripts/V2/GrabBox.cs
@@ -7,6 +7,61 @@ public class GrabBox : MonoBehaviour
     private List<GrabbableObject> _registeredGrabbables = new List<GrabbableObject>();
     public List<GrabbableObject> RegisteredGrabbables { get { return _registeredGrabbables; } }
 
+    private GrabbableObject _closestGrabbable;
+
+    private void FixedUpdate()
+    {
+        //if (_registeredGrabbables.Count == 0)
+        //    return;
+
+        //DebugLogManager.Instance.PrintLog(_registeredGrabbables.Count.ToString());
+
+
+        HighlightClosestGrabbable();
+    }
+
+    private void HighlightClosestGrabbable()
+    {
+        GrabbableObject closestGrabbable = CalculateClosestGrabbableObject();
+
+        if (closestGrabbable == _closestGrabbable)
+            return;
+
+
+        if (_closestGrabbable != null)
+        {
+            _closestGrabbable.StopHighlight();
+        }
+        if (closestGrabbable != null)
+        {
+            closestGrabbable.HighlightAsGrabbable();
+        }
+        _closestGrabbable = closestGrabbable;
+    }
+
+    private GrabbableObject CalculateClosestGrabbableObject()
+    {
+        if (_registeredGrabbables.Count == 0)
+            return null;
+        if (_registeredGrabbables.Count == 1)
+            return _registeredGrabbables[0];
+
+
+        GrabbableObject closestGrabbable = _registeredGrabbables[0];
+        foreach (var grabbable in _registeredGrabbables)
+        {
+            if (grabbable == closestGrabbable) continue;
+
+            float distanceGrabbable = Vector3.Distance(transform.position, grabbable.transform.position);
+            float distanceClosestGrabbable = Vector3.Distance(transform.position, closestGrabbable.transform.position);
+            if(distanceGrabbable < distanceClosestGrabbable)
+            {
+                closestGrabbable = grabbable;
+            }
+        }
+        return closestGrabbable;
+    }
+
     public void AddGrabbableObject(GrabbableObject grabbable)
     {
         _registeredGrabbables.Add(grabbable);
--- VR_Test1/Assets/Scripts/V2/GrabbableObject.cs
+++ VR_Test1/Assets/Scripts/V2/GrabbableObject.cs
@@ -5,10 +5,12 @@ public class GrabbableObject : MonoBehaviour
     private FixedJoint _attachedFixedJoint;
     private Rigidbody _rb;
     private Vector3 _previousPos;
+    private Material _material;
 
     private void Start()
     {
         _rb = GetComponent<Rigidbody>();
+        _material = GetComponent<MeshRenderer>().material;
     }
 
     private void FixedUpdate()
@@ -53,5 +55,24 @@ public class GrabbableObject : MonoBehaviour
         _attachedFixedJoint = null;
 
         _rb.velocity = velocity;
+    }
+
+    public void HighlightAsGrabbable()
+    {
+        DebugLogManager.Instance.PrintLog("Highlighting");
+
+        Color color = _material.color;
+        color.a = 0.1f;
+        _material.color = color;
+    }
+
+    public void StopHighlight()
+    {
+        DebugLogManager.Instance.PrintLog("stopping highlight");
+
+
+        Color color = _material.color;
+        color.a = 1f;
+        _material.color = color;
     }
 }
